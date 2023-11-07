3dcd24633503a71fccd68a6c13d3b1c2af19a87c
leo359531 <leos1359531@gmail.com>
2023-11-07 17:24:01 +0100

    better format of the code

--- apc_status.sh
+++ apc_status.sh
@@ -2,11 +2,11 @@
 
 if apcaccess | grep "STATUS   : ONBATT" 1>/dev/null;
 then
-  battery="$(apcaccess | grep BCHARGE | awk '{print $3;}')"↴
-  python3 /etc/bot/bot_api.py "ON BATTERY: $battery%"↴
-  if apcaccess | grep "STATUS   : ONLINE" 1>/dev/null;
-  then
-    python3 /etc/bot/bot_api.py "BATT ONLINE"↴
-    exit
-  fi
+    battery="$(apcaccess | grep BCHARGE | awk '{print $3;}')"↴
+    python3 /etc/bot/bot_api.py "ON BATTERY: $battery%"↴
+    if apcaccess | grep "STATUS   : ONLINE" 1>/dev/null;
+    then
+        python3 /etc/bot/bot_api.py "BATT ONLINE"↴
+        exit
+    fi
 fi

365a3f6c0b2ef70718834a46f5c389a6af358b8d
leo359531 <leos1359531@gmail.com>
2023-11-07 14:08:20 +0100

    WIP

--- apc_status.sh
+++ apc_status.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-while :
-do
   if apcaccess | grep "STATUS   : ONLINE";
   then
     battery="$(apcaccess | grep BCHARGE | awk '{print $3;}')"↴
     python3 /etc/bot/bot_api.py "ON BATTERY: $battery%"↴
-    echo hi
+    if apcaccess | grep "STATUS   : ONLINE";
+    then
+      python3 /etc/bot/bot_api.py "BATT ONLINE"↴
+      exit
+    fi
   fi
-sleep 1
-done

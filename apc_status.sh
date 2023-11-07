128390a652ad5cccc8126bc1346ca132ec74cf53
leo359531 <leos1359531@gmail.com>
2023-11-07 14:11:46 +0100

    added notification when back ONLINE

--- apc_status.sh
+++ apc_status.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-  if apcaccess | grep "STATUS   : ONLINE" 1>/dev/null;
+  if apcaccess | grep "STATUS   : ONBATT" 1>/dev/null;
   then
     battery="$(apcaccess | grep BCHARGE | awk '{print $3;}')"↴
     python3 /etc/bot/bot_api.py "ON BATTERY: $battery%"↴
